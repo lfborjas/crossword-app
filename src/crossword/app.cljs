ecbd981f3b872753c6345ef33e818dc608f4c184
Luis Borjas <luisfborjas@gmail.com>
2018-11-03 15:40:41 -0400

    removes example code, adds some initial state

--- src/crossword/app.cljs
+++ src/crossword/app.cljs
@@ -3,24 +3,29 @@
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]))
 
-(println "This text is printed from src/crossword/app.cljs. Go ahead and edit it and see reloading in action.")
-
-(defn multiply [a b] (* a b))
-
-
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:crossword-name "NYT Daily Mini"}))
+
+(def example-crossword [
+  [{}      {:n 1 :l \D} {:n 2 :l \U} {:n 3 :l \C} {:n 4 :l \K}]
+  [{:n 5 :l \D} {:l \R}      {:l \D}      {:l \R}      {:l \E}]
+  [{:n 6 :l \R} {:l \A}      {:l \D}      {:l \O}      {:l \N}]
+  [{:n 7 :l \O} {:l \C}      {:l \E}      {:l \A}      {:l \N}]
+  [{:n 8 :l \P} {:l \O}      {:l \R}      {:l \K}      {:l \Y}]
+])
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
-(defn hello-world []
+(defn cell [])
+
+(defn crossword []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/crossword/app.cljs and watch it change!"]])
+   [:h1 (:crossword-name @app-state)]
+   [:h3 "I guess this is cool. Is it fast?"]])
 
 (defn mount [el]
-  (reagent/render-component [hello-world] el))
+  (reagent/render-component [crossword] el))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
