9f7e558ec40dc5b22cd7280613831ffcd5ac033f
Luis Borjas <luisfborjas@gmail.com>
2018-11-04 23:04:34 -0500

    Rendering a single cell
    Following:
    * https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Getting_Started
    * https://www.w3.org/Graphics/SVG/IG/resources/svgprimer.html#coordinate
    * https://www.w3schools.com/graphics/svg_rect.asp

--- src/crossword/app.cljs
+++ src/crossword/app.cljs
@@ -4,7 +4,8 @@
    [reagent.core :as reagent :refer [atom]]))
 
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:crossword-name "NYT Daily Mini"}))
+(defonce app-state (atom {:crossword-name "NYT Daily Mini: Sunday, October 28"}))
+;(reset! app-state {:crossword-name "NYT Daily Mini: Sunday, October 28"})
 
 (def example-crossword [
   [{}      {:n 1 :l \D} {:n 2 :l \U} {:n 3 :l \C} {:n 4 :l \K}]
@@ -17,15 +18,34 @@
 (defn get-app-element []
   (gdom/getElement "app"))
 
-(defn cell [])
+;; https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Getting_Started
+;; https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Presentation
+;; https://www.w3.org/TR/css-color-3/#svg-color
 
-(defn crossword []
+(defn cell [{number :n, letter :l}]
+  [:g
+   [:rect {:width "60" :height "60" :x "3" :y "3"
+           :stroke "black" :stroke-width "3" :fill "blue" :fill-opacity "0.1"}]
+   [:text {:x "6" :y "15" :font-family "monospace"
+           :font-size "12" :fill "darkgray"}
+    number]
+   [:text {:x "34" :y "44" :font-family "sans-serif" :font-size "30"
+           :fill "black" :text-anchor "middle"}
+    letter]])
+
+(defn crossword [data]
+  (doseq [row data]
+    []))
+
+(defn puzzle-ui [crossword-data]
   [:div
    [:h1 (:crossword-name @app-state)]
-   [:h3 "I guess this is cool. Is it fast?"]])
+   [:svg  {:version "1.1" :baseProfile "full" :xmlns "http://www.w3.org/2000/svg"
+           :width "300" :height "300"}
+    [cell (-> crossword-data first second)]]])
 
 (defn mount [el]
-  (reagent/render-component [crossword] el))
+  (reagent/render-component [puzzle-ui example-crossword] el))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
